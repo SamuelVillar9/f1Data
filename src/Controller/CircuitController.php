a33d899be639f23d0aec556b480b74e7c4734829
Samuel Villar <samuelvillar9@gmail.com>
2024-12-29 19:42:14 +0100

    Implementar la inyección de dependencias en CircuitController y mejorar la visualización de circuitos en la plantilla.

--- src/Controller/CircuitController.php
+++ src/Controller/CircuitController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\CircuitRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CircuitController extends AbstractController
 {
-    #[Route('/circuitos', name: 'circuit')]
-    public function index(): Response
+
+    private $entityManager;
+
+    // Inyección del EntityManager en el constructor
+    public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/circuitos', name: 'circuit')]
+    public function index(Request $request, CircuitRepository $circuitRepository): Response
+    {
+
+        $circuits = $circuitRepository->findAll();
+
         return $this->render('circuit/index.html.twig', [
-            'controller_name' => 'CircuitController',
+            'circuits' => $circuits,
         ]);
     }
 }
